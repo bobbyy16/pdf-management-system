8068c8942b69dee383573db333fdae2bf39dc6b9
bobbyy16 <bobbyyyyy16@gmail.com>
2025-05-23 15:02:56 +0530

    Refactor Google Drive authentication to use environment variables for credentials

--- server/config/googleDriveConfig.js
+++ server/config/googleDriveConfig.js
@@ -4,7 +4,19 @@ const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: path.join(__dirname, "../google-service-account.json"),
+  credentials: {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_CERT_URL,
+    universe_domain: process.env.GOOGLE_UNIVERSE_DOMAIN,
+  },
   scopes: ["https://www.googleapis.com/auth/drive"],
 });
 
